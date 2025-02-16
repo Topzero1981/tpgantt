446e9c4c7ea58bfa1029a4502e81fa874525dee9
Topzero1981 <abonilla@zakatran.com>
2025-02-16 12:27:01 +0100

    Update ganttWidget.js
    removed the Scroll Toggler

--- ganttWidget.js
+++ ganttWidget.js
@@ -1,40 +1,41 @@
-// ganttWidget.js
 (function () {
-  window.GanttWidget = window.GanttWidget || {};
-
-  function loadScript(url, callback) {
-    var script = document.createElement("script");
-    script.src = url;
-    script.onload = callback;
-    document.head.appendChild(script);
-  }
-
-  // Updated list of modules including widgetScrollToggler.js
-  var modules = [
-    "dependencyLoader.js",
-    "widgetUtils.js",
-    "widgetStylesUI.js",
-    "widgetRenderer.js",
-    "widgetExcel.js",
-    "widgetScrollToggler.js"  // New module for toggling scrollbars
-  ];
-
-  function loadModules(index, finalCallback) {
-    if (index < modules.length) {
-      loadScript(modules[index], function () {
-        loadModules(index + 1, finalCallback);
-      });
-    } else {
-      finalCallback();
+    // Create a global namespace for our widget.
+    window.GanttWidget = window.GanttWidget || {};
+  
+    // Helper to load a script dynamically.
+    function loadScript(url, callback) {
+      var script = document.createElement("script");
+      script.src = url;
+      script.onload = callback;
+      document.head.appendChild(script);
     }
-  }
-
-  loadModules(0, function () {
-    GanttWidget.loadDependencies(function () {
-      // Initialize the widget.
-      GanttWidget.init();
-      // Add the Scroll Toggle Button.
-      GanttWidget.addScrollToggleButton();
+  
+    // List of module files to load.
+    var modules = [
+      "dependencyLoader.js",
+      "widgetUtils.js",
+      "widgetStylesUI.js",
+      "widgetRenderer.js",
+      "widgetExcel.js"
+    ];
+  
+    // Load the modules sequentially.
+    function loadModules(index, finalCallback) {
+      if (index < modules.length) {
+        loadScript(modules[index], function () {
+          loadModules(index + 1, finalCallback);
+        });
+      } else {
+        finalCallback();
+      }
+    }
+  
+    loadModules(0, function () {
+      // Once our modules are loaded, load D3 and XLSX via our dependency loader.
+      GanttWidget.loadDependencies(function () {
+        // Now initialize the widget.
+        GanttWidget.init();
+      });
     });
-  });
-})();
+  })();
+  
